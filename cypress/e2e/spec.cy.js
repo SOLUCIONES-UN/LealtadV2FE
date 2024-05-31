8bbd2ed6fdc4de9674deca34b8a1e0dee3e06c44
elenagaal <129307850+elenagaal@users.noreply.github.com>
2024-05-31 16:34:52 -0600

    cambiso en estructura testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
+    cy.viewport(1200, 800);
     cy.visit('http://127.0.0.1:5501/pages/login.html')
     cy.get('#formLogin > :nth-child(1)').type('JEstivenA')
     cy.get('#login-password').type('admin')
     cy.get('#btnLoggin').click()
-    cy.get('.d-xl-none > .nav-item > .nav-link').dblclick();
-
+    cy.get('.active > .d-flex').click()
+   
 
 
 
