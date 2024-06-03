b63274b8e664c604c170158f21afe280b729498f
elenagaal <129307850+elenagaal@users.noreply.github.com>
2024-06-03 11:17:00 -0600

    correcciones

--- js/usuario.js
+++ js/usuario.js
@@ -289,15 +289,15 @@ const getUsuarios = () => {
             feather.icons["more-vertical"].toSvg({ class: "font-small-4" }) +
             "</a>" +
             '<div class="dropdown-menu dropdown-menu-right">' +
-            '<a href="#" onclick="OpenEdit(\'' +
+            '<a href="#" onclick="OpenEdit(' +
             data +
-            '\')" class="btn_edit dropdown-item">' +
+            ')" class="btn_edit dropdown-item">' +
             feather.icons["archive"].toSvg({ class: "font-small-4 mr-50" }) +
             " Actualizar" +
             "</a>" +
-            '<a href="#" onclick="OpenDelete(\'' +
+            '<a href="#" onclick="OpenDelete(' +
             data +
-            '\')" class="btn_delete dropdown-item">' +
+            ')" class="btn_delete dropdown-item">' +
             feather.icons["trash-2"].toSvg({ class: "font-small-4 mr-50" }) +
             " Inhabilitar" +
             "</a>" +
